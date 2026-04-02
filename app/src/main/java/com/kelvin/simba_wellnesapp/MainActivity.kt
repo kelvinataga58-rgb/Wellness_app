b83936be6297b9cd91dc2a6b9fff05af1a518715
kelvinataga58-rgb <kelvinataga58@gmail.com>
2026-04-02 16:13:10 +0300

    We have added intent and the navigation from one site to another

---
+++ app/src/main/java/com/kelvin/simba_wellnesapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.kelvin.simba_wellnesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+//        Finding ids using find view by id from the layout
+//        create a variable to store the button
+
+        val recipe=findViewById<Button>(R.id.recipes)
+//        set onClick listener
+
+        recipe.setOnClickListener {
+
+//            enable our intent
+
+            val recipeIntent= Intent(applicationContext, HealthyRecipes::class.java)
+            startActivity(recipeIntent)
+        }
+
+
+//        nutrition intent
+
+        val nutrition= findViewById<Button>(R.id.nutrition)
+
+        nutrition.setOnClickListener {
+
+            val nutritionIntent=Intent(applicationContext, NutritionAdvice::class.java)
+            startActivity(nutritionIntent)
+        }
+
+
+//        Meditation intent
+
+        val meditation= findViewById<Button>(R.id.meditation)
+
+        meditation.setOnClickListener {
+            val meditationIntent= Intent(applicationContext, Meditation::class.java)
+            startActivity(meditationIntent)
+        }
+
+
+
+    }
+}
